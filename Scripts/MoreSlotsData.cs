ef5f17606e1a6b15afef4f1dceb43c72a6979ac4
Wully <wully@robb.scot>
2022-07-03 02:31:17 +0100

    reduced data class

--- Scripts/MoreSlotsData.cs
+++ Scripts/MoreSlotsData.cs
@@ -12,24 +12,11 @@ namespace Wully.MoreSlots
 		public Vector3 localPosition;
 		public Vector3 localRotation;
 		public string ragdollPartName;
-		public bool enableCollisions = false;
-		public bool ignoreCollisionsWithPlayer = false;
-		public bool ignoreCollisionsWithPlayerHands = false;
-		public bool ignoreCollisionsWithEquippedItems = false;
-		public bool physicsParent = false;
 		public bool scaleToPlayer = false;
-		public string rotateRelativeTo;
-		public Dir holderDir;
-		public Dir relativeToDir;
-		public Dir relativePartAxis;
 
-		public override string ToString() {
-			return $"{nameof(holderDataId)}: {holderDataId}, {nameof(handSide)}: {handSide}, {nameof(localPosition)}: {localPosition}, {nameof(localRotation)}: {localRotation}, {nameof(ragdollPartName)}: {ragdollPartName}, {nameof(enableCollisions)}: {enableCollisions}, {nameof(ignoreCollisionsWithPlayer)}: {ignoreCollisionsWithPlayer}, {nameof(ignoreCollisionsWithPlayerHands)}: {ignoreCollisionsWithPlayerHands}, {nameof(ignoreCollisionsWithEquippedItems)}: {ignoreCollisionsWithEquippedItems}, {nameof(physicsParent)}: {physicsParent}, {nameof(scaleToPlayer)}: {scaleToPlayer}";
-		}
-
-		public enum Dir {
-			up,down,left,right,forward,back
-
+		public override string ToString()
+		{
+			return $"{nameof(holderDataId)}: {holderDataId}, {nameof(handSide)}: {handSide}, {nameof(localPosition)}: {localPosition}, {nameof(localRotation)}: {localRotation}, {nameof(ragdollPartName)}: {ragdollPartName}, {nameof(scaleToPlayer)}: {scaleToPlayer}";
 		}
 	}
 }
